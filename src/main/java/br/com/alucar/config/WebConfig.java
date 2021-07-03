d3a0e985f449e049541c4ad4bf4dc55f2315a395
Luiz Faleiros <luiz.faleiros@iteris.com.br>
2021-07-03 16:37:25 -0300

    Criando o content

--- src/main/java/br/com/alucar/config/WebConfig.java
+++ src/main/java/br/com/alucar/config/WebConfig.java
@@ -1,19 +1,28 @@
 package br.com.alucar.config;
 
+import br.com.alucar.domain.converters.serialization.YamlsToHttp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMessage;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
+import java.util.List;
 
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    private static final MediaType APPLICATION_YAML = MediaType.valueOf("application/x-yaml");
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -27,5 +36,21 @@ public class WebConfig implements WebMvcConfigurer {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**",corsConfig);
         return source;
+    }
+
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+        converters.add(new YamlsToHttp());
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false)
+                .favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", APPLICATION_YAML);
     }
 }
