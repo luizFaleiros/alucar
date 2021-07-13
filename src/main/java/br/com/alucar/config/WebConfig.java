6a35372760eb01f1b653baa351906321e09ec872
Luiz Faleiros <luiz.faleiros@iteris.com.br>
2021-07-12 23:06:00 -0300

    Mandando a boa

--- src/main/java/br/com/alucar/config/WebConfig.java
+++ src/main/java/br/com/alucar/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*/*")
+                .allowedOrigins("*")
                 .allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
